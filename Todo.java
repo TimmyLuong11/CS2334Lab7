84448bb9da36c4027b14e233664a170389734849
Timmy <timothy.luong@ou.edu>
2019-03-10 15:40:27 -0500

    Beginning Code

---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-17
+OU ID_______________: 113401608
+OU EMAIL____________: timothy.luong@ou.edu
+
+Tasks:
+1. Finish Circle.java
+2. Finish Rectangle and Square
+3. Finish ShapeSorterTest and ShapeTest and ShapeSorter
+4. Finish ShapeAreaComparator and ShapePerimeterComparator
+5. Finish EqualateralTriangle and Polygon and Trapezoid 
+
+Estimated Time (minutes):
+1. 10
+2. 15
+3. 30
+4. 30
+5. 60
+
+Actual Time (minutes):
+1. 5
+2. 10
+3. 60
+4. 180
+5. 120
+*/}
